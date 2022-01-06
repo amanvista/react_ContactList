076dc4fed5f61a97f7e9b318c5bc39a5590b070a
Aman Bisht <amanvista@gmail.com>
2022-01-06 17:57:51 +0530

    Add and Delete Working

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,42 @@ import './App.css';
 import Header from './Header'
 import AddContact from './AddContact'
 import ContactList from './ContactList'
-import React,{useState} from 'react'
-
+import React,{useState, useEffect} from 'react'
+let contact_id = 0;
 function App() {
-  const [contacts, setContact] = useState([])
   
+  const [contacts, setContacts] = useState([])
+  const addContactHandler = (contact) => {
+    if(contacts.length == 0){
+     
+      setContacts([...contacts, { id: ++contact_id , ...contact}])
+      
+    }
+    else{
+      
+      setContacts([...contacts, { id: ++contact_id , ...contact}])
+      console.log(contacts)
+    }
+  }
+  const removeContactHandler = (id)=>{
+    const newContactList = contacts.filter( (contact) =>{
+      return contact.id !== id
+    })
+    setContacts(newContactList)
+  }
+  // useEffect(()=>{
+  //   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts))
+  // }, [contacts] )
+  // useEffect(()=>{
+  //   const retreivedContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+  //   setContacts(retreivedContacts)
+
+  // },[contacts])
   return (
     <div className="ui container">
       <Header/>
-      <AddContact/>
-      <ContactList contacts = {contacts} /> 
+      <AddContact addContactHandler={addContactHandler}/>
+      <ContactList contacts = {contacts} getContactId={removeContactHandler} /> 
     </div>
   );
 }
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import ContactCard from './ContactCard'
 const ContactList = (props) =>{
-
+    const deleteContactHandler = (id)=>{
+        props.getContactId(id)
+    }
     const renderContactList = props.contacts.map((contact)=>{
         return (
-            <ContactCard contact={contact}/>
+            <ContactCard contact={contact} clickHandler={deleteContactHandler}/>
         )
     }
-
     )
+
     return (
         <div className="ui celled list">
             {renderContactList}
